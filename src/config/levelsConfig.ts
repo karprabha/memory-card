34263fe1ef7a8e1f895c9b140bff9a50af154db3
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2023-08-29 22:07:44 +0530

    update level config

--- src/config/levelsConfig.ts
+++ src/config/levelsConfig.ts
@@ -1,7 +1,7 @@
 export const levels = [
-    { numberOfPokemonToFetch: 5, highestAllowedPokemonId: 649 },
+    { numberOfPokemonToFetch: 4, highestAllowedPokemonId: 649 },
+    { numberOfPokemonToFetch: 7, highestAllowedPokemonId: 649 },
     { numberOfPokemonToFetch: 10, highestAllowedPokemonId: 649 },
-    { numberOfPokemonToFetch: 15, highestAllowedPokemonId: 649 },
-    { numberOfPokemonToFetch: 20, highestAllowedPokemonId: 649 },
-    { numberOfPokemonToFetch: 25, highestAllowedPokemonId: 649 },
+    { numberOfPokemonToFetch: 13, highestAllowedPokemonId: 649 },
+    { numberOfPokemonToFetch: 16, highestAllowedPokemonId: 649 },
 ];
