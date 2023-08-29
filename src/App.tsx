6117507248e102cd8aea20857be70b99189ec31e
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2023-08-29 21:47:13 +0530

    add help section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import Game from "./components/Game";
 import Menu from "./components/Menu";
+import pikachuURL from "./assets/images/pikachu.png";
 
 const App: React.FC = () => {
     const [isGameStarted, setIsGameStarted] = useState(false);
+    const [isHelpVisible, setIsHelpVisible] = useState(false);
 
     const handleStartGame = () => {
         setIsGameStarted(true);
@@ -11,6 +13,10 @@ const App: React.FC = () => {
 
     const handleReturnToMenu = () => {
         setIsGameStarted(false);
+    };
+
+    const toggleHelpVisibility = () => {
+        setIsHelpVisible(!isHelpVisible);
     };
 
     return (
@@ -22,6 +28,28 @@ const App: React.FC = () => {
                 ) : (
                     <Menu onStartGame={handleStartGame} />
                 )}
+            </div>
+            <div className="game-options-container">
+                <div className="music-btn">
+                    <button type="button">🔊</button>
+                    {/* <button type="button">🔈</button> */}
+                </div>
+                <div className="help">
+                    <div
+                        className={`how-to-play ${
+                            isHelpVisible ? "visible" : "hidden"
+                        }`}
+                    >
+                        <ul className="info">
+                            <li>Don't click on the same card twice!</li>
+                            <li>Click on Pokémon logo to go back.</li>
+                        </ul>
+                        <img src={pikachuURL} alt="pikachu" />
+                    </div>
+                    <button type="button" onClick={toggleHelpVisibility}>
+                        ❔
+                    </button>
+                </div>
             </div>
         </>
     );
